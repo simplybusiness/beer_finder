325b194b0c6de1c48f550a4d0aeec626d78ae1fa
Pete <pete.williams@simplybusiness.co.uk>
2018-10-10 15:54:59 +0100

    dont include the in sort

--- app/domain/outlet_repository.rb
+++ app/domain/outlet_repository.rb
@@ -1,5 +1,15 @@
 class OutletRepository
   def self.get_outlets
     outlets = Outlet.all
+    sort_by_name(outlets.to_a)
+  end
+
+  def self.sort_by_name(outlets)
+
+    outlets.sort! { |x,y| no_definite_article(x.name) <=> no_definite_article(y.name) }
+  end
+
+  def self.no_definite_article(text)
+    text.sub /^The /, ''
   end
 end
