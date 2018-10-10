b378f498ce1548f9068006f62345fb3bedf86dc8
Paweł Dawczak <pawel.dawczak@gmail.com>
2018-10-10 11:01:56 +0100

    Add ActiveRecord and PosrgreSQL support

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
-require "action_controller/railtie"
-require "sprockets/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,4 +71,6 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  config.active_record.dump_schema_after_migration = false
 end
