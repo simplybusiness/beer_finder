54a0b99aee26d0909655ffb9c65bf2b296a368a8
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2018-10-11 11:20:44 +0100

    Return villages coords if postcode not working with geolocator

--- app/queries/coordinates_query.rb
+++ app/queries/coordinates_query.rb
@@ -2,6 +2,7 @@ class CoordinatesQuery
   def self.for_postcode(postcode)
     results = Geocoder.search(postcode)
     return results.first.coordinates if results.present?
-    []
+    # return villages coord if not found
+    [51.478605, -0.02474]
   end
 end
--- spec/queries/coordinates_spec.rb
+++ spec/queries/coordinates_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe CoordinatesQuery do
     expect(coordinates).to eq [51.515321763772, -0.090416409760255]
   end
 
-  it 'returns an empty array if postcode not found' do
-    expect(get_coordinates.for_postcode('SW1Y 4AH')).to eq([])
+  it 'returns villages coords if postcode not found' do
+    villages_cords = [51.478605, -0.02474]
+    expect(get_coordinates.for_postcode('SW1Y 4AH')).to eq(villages_cords)
   end
 end
