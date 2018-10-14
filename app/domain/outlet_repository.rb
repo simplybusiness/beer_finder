3b5d46eb164641476cdcbc9411384b5ba79c3283
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2018-10-14 12:13:26 +0100

    Render the distance from postcode
    Don't need to add anything to the model if we do this is a more
    functional way.

--- app/domain/outlet_repository.rb
+++ app/domain/outlet_repository.rb
@@ -9,7 +9,7 @@ class OutletRepository
       return sort_by_name(outlets.to_a) unless postcode.present?
 
       location = CoordinatesQuery.for_postcode(postcode)
-      sort_by_distance(outlets.to_a, location)
+      add_distances_and_sort(outlets.to_a, location)
     end
 
     private
@@ -22,19 +22,16 @@ class OutletRepository
       text.sub /^The /, ''
     end
 
-    def sort_by_distance(outlets, location)
-      add_distances(outlets, location)
-      outlets.sort! { |x, y| x.distance <=> y.distance }
-    end
-
-    def add_distances(outlets, location)
-      outlets.each { |o| add_distance(o, location) }
-    end
-
-    def add_distance(outlet, location)
-      outlet_location = [ outlet.lat, outlet.long ]
-      outlet.distance = Utils.distance_in_miles(outlet_location, location).round
-      outlet.distance_units = ' miles away'
+    def add_distances_and_sort(outlets, location)
+      outlets.map do |outlet|
+        outlet_location = [ outlet.lat, outlet.long ]
+        outlet.attributes.merge(
+          {
+            distance: Utils.distance_in_miles(outlet_location, location).round(2),
+            distance_units: ' miles away'
+          }
+        )
+      end.sort { |x, y| x[:distance] <=> y[:distance] }
     end
   end
 end
--- app/models/outlet.rb
+++ app/models/outlet.rb
@@ -1,7 +1,5 @@
 class Outlet < ApplicationRecord
 
-  attr_accessor :distance
-  attr_accessor :distance_units
   has_many :stock_items
 
   def recent_stock_items
