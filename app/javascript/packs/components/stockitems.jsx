d55ac238d1301e6f5123b7de1b758b20ace7debb
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2018-10-28 13:52:11 +0000

    Use implicit return in stock items
    and set default array of component is not given outlets

--- app/javascript/packs/components/stockitems.jsx
+++ app/javascript/packs/components/stockitems.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import moment from 'moment'
 
-const StockItems = ({ items = [] }) => {
-  return items.map(item => {
-    return (
-      <li className="list-group-item d-flex justify-content-between align-items-center" key={item.id}>
-        <div><span><img src={item.icon_image} width="24" height="24"/></span> {item.name}</div>
-        <small>{moment(item.delivery_date).fromNow()}</small>
-      </li>
-    );
-  });
-}
+const StockItems = ({ items = [] }) =>  items.map(item => (
+  <li className="list-group-item d-flex justify-content-between align-items-center" key={item.id}>
+    <div><span><img src={item.icon_image} width="24" height="24"/></span> {item.name}</div>
+    <small>{moment(item.delivery_date).fromNow()}</small>
+  </li>
+));
+
 
 export default StockItems
