e8dfba7f35c4cae0a05d44ec91cbbca0443d3648
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2018-11-02 10:59:43 +0000

    Add async await

--- app/javascript/packs/components/stockitems.jsx
+++ app/javascript/packs/components/stockitems.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import moment from 'moment'
 
-const StockItems = ({ items = [] }) =>  items.map(item => (
+const StockItems = ({ items = [] }) => items.map(item => (
   <li className="list-group-item d-flex justify-content-between align-items-center" key={item.id}>
     <div><span><img src={item.icon_image} width="24" height="24"/></span> {item.name}</div>
     <small>{moment(item.delivery_date).fromNow()}</small>
